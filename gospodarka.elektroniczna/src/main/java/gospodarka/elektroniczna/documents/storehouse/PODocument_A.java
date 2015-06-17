1d393ed40528a5be3158249928307423fa7090c8
Kasia Jee <kasia.jedrek@gmail.com>
2015-06-17 09:05:38 +0200

    storehouse add prod.order

---
+++ gospodarka.elektroniczna/src/main/java/gospodarka/elektroniczna/documents/storehouse/PODocument_A.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package gospodarka.elektroniczna.documents.storehouse;
+
+import gospodarka.elektroniczna.dto.storehouse.PODocument;
+
+/**
+ * @author Kasia
+ *
+ */
+public class PODocument_A extends AbstractStoreHouseDocument<PODocument> {
+
+    private static final long serialVersionUID = 1L;
+
+}
